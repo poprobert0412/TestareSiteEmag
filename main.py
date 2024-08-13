4e3d391ac1132750f66108d83aa59adaf620e418
Robert <poprobert1999@yahoo.com>
2024-08-13 18:21:55 +0300

    Test case esuat deoarece site ul emag are un program anti testare!

--- main.py
+++ main.py
@@ -9,7 +9,12 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 
 class SiteEmag(unittest.TestCase):
-
+    cokkies = (By.XPATH, "//button[@class=\"btn btn-primary btn-block js-accept gtm_h76e8zjgoo\"]")
+    search_box = (By.ID, "searchboxTrigger")
+    iphone_15_pro_max = (By.XPATH, "//div[contains(@data-url, \"https://www.emag.ro/telefon-mobil-apple-iphone-15-pro-max\")]")
+    iphone_256 = (By.XPATH, "//div[contains(text(), \"256\")]")
+    iphone_512 = (By.XPATH, "//div[contains(text(), \"512\")]")
+    iphone1TB = (By.XPATH, "//div[contains(text(), \"1 TB\")]")
     def setUp(self): #codul pentru inceputul testarii ca sa putem lua site ul pe care o sa facem urmatoarea testare
         chrome_options = Options()
         chrome_options.add_argument("--disable-search-engine-choice-screen")#Acest cod este pentru problema cu default browser
@@ -19,4 +24,23 @@ class SiteEmag(unittest.TestCase):
     def tearDown(self):#Codul care o sa ruleze la sfarsitul testarii
         self.chrome.quit()
 
-    def test_
+    def test_numar_iphone_15_pro_max(self):
+ #Punem aceea steluta ca sa despachetam booleanul din cookies
+        self.chrome.find_element(*self.search_box).send_keys("Iphone 15 pro max")
+        self.chrome.find_element(*self.search_box).send_keys(Keys.ENTER)
+        WebDriverWait(self.chrome, 3).until(EC.presence_of_element_located(self.iphone_15_pro_max))
+        phones = self.chrome.find_elements(*self.iphone_15_pro_max)
+        iphone_15_sum = len(phones)
+        print(f"Am gasit {iphone_15_sum} de telefoane Iphone 15 pro max!")
+
+    def test_iphone_GB(self):
+        self.chrome.find_element(*self.search_box).send_keys("Iphone 15 pro max")
+        self.chrome.find_element(*self.search_box).send_keys(Keys.ENTER)
+        WebDriverWait(self.chrome, 3).until(EC.presence_of_element_located(self.iphone_15_pro_max))
+        self.chrome.find_element(*self.iphone_15_pro_max).click()
+        phone256 = self.chrome.find_element(*self.iphone_256)
+        phone512 = self.chrome.find_element(*self.iphone_512)
+        phone1TB = self.chrome.find_element(*self.iphone1TB)
+
+        assert phone256, "Avem disponibil telefonul de 256 de gb!"
+#NU MAI MERGE SITE UL DEOARECE ARE ANTI TESTARE
